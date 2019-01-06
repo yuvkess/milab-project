1e92b86b501078761aec7154bc370d7ffd4e1b10
yuvkess <yuvalkessel@gmail.com>
2019-01-06 21:31:50 +0200

    added requests with gps data to server

--- GPS_service-master/app/src/main/java/testing/gps_service/GPS_Service.java
+++ GPS_service-master/app/src/main/java/testing/gps_service/GPS_Service.java
@@ -11,11 +11,27 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GPS_Service extends Service {
 
     private LocationListener listener;
     private LocationManager locationManager;
+    private RequestQueue requestQueue;
+    private static final String REQUEST_URL = "http://10.0.2.2:3000/gps_data";
 
     @Nullable
     @Override
@@ -26,13 +42,35 @@ public class GPS_Service extends Service {
     @SuppressLint("MissingPermission")
     @Override
     public void onCreate() {
-
+            requestQueue = Volley.newRequestQueue(this);
             listener = new LocationListener() {
                 @Override
-                public void onLocationChanged(Location location) {
+                public void onLocationChanged(final Location location) {
                     Intent i = new Intent("location_update");
                     i.putExtra("coordinates",location.getLongitude()+" "+location.getLatitude());
                     sendBroadcast(i);
+
+                    // Sending to server
+                    Map<String, String> params = new HashMap();
+                    params.put("longitude", String.valueOf(location.getLongitude()));
+                    params.put("latitude", String.valueOf(location.getLatitude()));
+
+                    JSONObject parameters = new JSONObject(params);
+
+                    JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, REQUEST_URL, parameters, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            //TODO: handle success
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            error.printStackTrace();
+                            //TODO: handle failure
+                        }
+                    });
+
+                    requestQueue.add(jsonRequest);
                 }
 
                 @Override
@@ -54,7 +92,6 @@ public class GPS_Service extends Service {
             };
 
         locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-
         //noinspection MissingPermission
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3000,0,listener);
 
